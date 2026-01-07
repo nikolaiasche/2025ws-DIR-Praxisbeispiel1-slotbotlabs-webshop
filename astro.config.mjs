9a01141320c26e7cd4b12b298015f0cba4873f9a
Nikolai <e12318680@student.tuwien.ac.at>
2026-01-07 16:15:36 +0100

    commit bug fixes regarding path and action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nikolaiasche.github.io',
-  base: '/2025ws-DIR-Praxisbeispiel1-slotbotlabs-webshop',
+  base: '/2025ws-DIR-Praxisbeispiel1-slotbotlabs-webshop/',
 });
